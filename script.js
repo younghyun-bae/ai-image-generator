9ff6ac699ab66a4872a4f426c9c95b2203704041
Younghyun <yhbae97@gmail.com>
2024-06-25 18:40:10 +0200

    fix bug - added extra img

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const generateBtn = document.querySelector('#generateBtn');
 const gridContainer = document.querySelector('#gridContainer');
 
 generateBtn.addEventListener('click', () => {
-  const randomNumber = Math.floor(Math.random() * 1000) + 1;
-  const imgUrl = `https://picsum.photos/300?random=${randomNumber}`;
-
-  const img = document.createElement('img');
-  img.src = imgUrl;
-
-  gridContainer.appendChild(img);
 
   if (gridContainer.children.length >= 30) {
     const confirmed = confirm("You have reached the maximum number of images. Would you like to delete all of them?");
     if (confirmed) {
       gridContainer.innerHTML = "";
     }
+  } else {  
+    const randomNumber = Math.floor(Math.random() * 1000) + 1;
+    const imgUrl = `https://picsum.photos/300?random=${randomNumber}`;
+  
+    const img = document.createElement('img');
+    img.src = imgUrl;
+  
+    gridContainer.appendChild(img);
   }
 });
