d9405f45eaae48612212881c5e5a3017597e4f32
Younghyun <yhbae97@gmail.com>
2024-06-25 17:58:51 +0200

    add feature - Generate images

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-const generateBtn = document.querySelector('#generateBtn')
+const generateBtn = document.querySelector('#generateBtn');
+const gridContainer = document.querySelector('#gridContainer');
 
 generateBtn.addEventListener('click', () => {
-  console.log('func wokrs if the button is clicked');
-})
+  const randomNumber = Math.floor(Math.random() * 1000) + 1;
+  const imgUrl = `https://picsum.photos/300?random=${randomNumber}`;
+
+  const img = document.createElement('img');
+  img.src = imgUrl;
+
+  gridContainer.appendChild(img);
+});
